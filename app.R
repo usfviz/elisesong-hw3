23915c923ca0459f17139c971d329df64bfb78c9
EliseSong <nsong4@dons.usfca.edu>
2017-04-26 00:07:02 -0700

    Update app.R

--- app.R
+++ app.R
@@ -3,38 +3,10 @@ library("ggplot2")
 library("GGally")
 library("plotly")
 
-setwd("/Users/Elise/DataViz/HW3")
+#setwd("/Users/Elise/DataViz/HW3")
 rm(list = ls())
 cat("\014") 
-
-library("ggplot2")
-library(shiny)
-library(plotly)
-
 df <- read.csv("dataset_Facebook.csv", sep=";", header=T)
-
-# Bubble Plot
-# x=like, y=Lifetime.Post.Total.Impressions, size=Engaged.Users, interaction_1=type
-
-# Scatter Plot Matrix
-# x_facets=weekday, y_facets=type, x=interaction(like,comment,share), y=interaction(Lifetime...)
-
-# Parallel Coordinates Plot
-# x=c(comment, like, share, total), y=c(lifetime.etc), interaction_1=type
-
-# # Define UI for application
-# ui <- fluidPage(
-#   headerPanel("Facebook Metrics"),
-#   sidebarPanel(
-#     sliderInput('Post.Month','Select Month', value = 1, min = 1, 
-#                 max = 12, step = 1)
-#   ),
-#   mainPanel(tabsetPanel(
-#     tabPanel("BubblePlot", plotlyOutput("BubblePlot")),
-#     tabPanel("ScatterPlot Matrix", plotOutput("ScatterPlot_Matrix")),
-#     tabPanel("Parallel", plotlyOutput("Parallel"))
-#   ))
-# )
 
 ui <- fluidPage(
   headerPanel("Facebook Visualization"),
@@ -46,18 +18,10 @@ ui <- fluidPage(
     )
   )
 )
-
-#ggpairs(df, columns = c(16:18))
-
-
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   output$bubble <- renderPlotly({
     ggplot(df, aes(like, Lifetime.Post.Total.Impressions)) +
       geom_point(aes(size = Total.Interactions, colour = Type))
-    # theme(plot.title = element_text(hjust = 0.5), legend.justification = c("right", "top")) 
-    # scale_size(range = c(1, 6)*input_size()) +
-    # guides(size= "none")
   })
   output$scatter <- renderPlotly({
     ggpairs(df, columns = c(16:19), columnLabels = colnames(df)[16:19])
@@ -67,5 +31,4 @@ server <- function(input, output) {
   })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
